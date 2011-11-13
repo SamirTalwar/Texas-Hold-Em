5c23bf2cce7fd9fc31343932d3c55051e712a1a2
Samir Talwar <samir@noodlesandwich.com>
2011-11-13 21:16:21 +0000

    Got rid of the TexasHoldEm object, as it was just wrapping Hand.

--- src/main/java/com/noodlesandwich/workshops/texasholdem/TexasHoldEm.java
+++
@@ -1,7 +0,0 @@
-package com.noodlesandwich.workshops.texasholdem;
-
-public final class TexasHoldEm {
-    public String rank(final String hand) {
-        return new Hand(hand).rank();
-    }
-}
--- src/test/java/com/noodlesandwich/workshops/texasholdem/HandTest.java
+++ src/test/java/com/noodlesandwich/workshops/texasholdem/HandTest.java
@@ -5,21 +5,19 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public final class TexasHoldEmTest {
-    private final TexasHoldEm texasHoldEm = new TexasHoldEm();
-
+public final class HandTest {
     @Test public void
     detects_a_hand_is_a_high_card() {
-        assertThat(texasHoldEm.rank("2c 5d 6c 8s Td Ks Ah"), is("High Card"));
+        assertThat(new Hand("2c 5d 6c 8s Td Ks Ah").rank(), is("High Card"));
     }
 
     @Test public void
     detects_a_pair() {
-        assertThat(texasHoldEm.rank("4d 8c 9s Td Th Qs Kc"), is("Pair"));
+        assertThat(new Hand("4d 8c 9s Td Th Qs Kc").rank(), is("Pair"));
     }
 
     @Test public void
     detects_two_pair() {
-        assertThat(texasHoldEm.rank("3s 5c 5d 8h 9h 9d Jc"), is("Two Pair"));
+        assertThat(new Hand("3s 5c 5d 8h 9h 9d Jc").rank(), is("Two Pair"));
     }
 }
