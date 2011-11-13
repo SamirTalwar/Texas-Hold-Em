abbbde0671961b29653943173dfe7ee0602bd826
Samir Talwar <samir@noodlesandwich.com>
2011-11-13 19:14:27 +0000

    Made the test pass.

--- src/main/java/com/noodlesandwich/workshops/texasholdem/TexasHoldEm.java
+++ src/main/java/com/noodlesandwich/workshops/texasholdem/TexasHoldEm.java
@@ -2,6 +2,6 @@ package com.noodlesandwich.workshops.texasholdem;
 
 public final class TexasHoldEm {
     public String rank(final String hand) {
-        throw new UnsupportedOperationException();
+        return "High Card";
     }
 }
--- src/test/java/com/noodlesandwich/workshops/texasholdem/TexasHoldEmTest.java
+++ src/test/java/com/noodlesandwich/workshops/texasholdem/TexasHoldEmTest.java
@@ -1,13 +1,11 @@
 package com.noodlesandwich.workshops.texasholdem;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public final class TexasHoldEmTest {
-    @Ignore("pending implementation")
     @Test public void
     detects_a_hand_is_a_high_card() {
         final TexasHoldEm texasHoldEm = new TexasHoldEm();
