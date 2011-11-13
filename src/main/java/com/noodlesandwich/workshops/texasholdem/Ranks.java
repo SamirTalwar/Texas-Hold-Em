496239db4d04d8d8ff3251c8ff3a8ff8fd2675c8
Samir Talwar <samir@noodlesandwich.com>
2011-11-13 21:45:24 +0000

    Separated out the different ranking criteria.

--- src/main/java/com/noodlesandwich/workshops/texasholdem/Ranks.java
+++ src/main/java/com/noodlesandwich/workshops/texasholdem/Ranks.java
@@ -1,26 +1,50 @@
 package com.noodlesandwich.workshops.texasholdem;
 
 import com.noodlesandwich.workshops.functional.FunctionalList;
+import com.noodlesandwich.workshops.functional.Predicate;
 
 public class Ranks {
     public static String rank(final FunctionalList<Card> hand) {
-        return rankForPair(hand, Card.NULL);
+        return TWO_PAIR.matches(hand)
+                   ? "Two Pair"
+                   : PAIR.matches(hand)
+                       ? "Pair"
+                       : "High Card";
     }
 
-    private static String rankForPair(final FunctionalList<Card> hand, final Card last) {
-        return hand.isEmpty()
-                   ? "High Card"
-                   : last.hasTheSameNumberAs(hand.head())
-                       ? rankForTwoPair(hand.tail(), Card.NULL)
-                       : rankForPair(hand.tail(), hand.head());
-    }
+    public static Predicate<FunctionalList<Card>> PAIR = new Predicate<FunctionalList<Card>>() {
+        @Override public boolean matches(final FunctionalList<Card> cards) {
+            return rankForPair(cards, Card.NULL);
+        }
 
-    private static String rankForTwoPair(final FunctionalList<Card> hand, final Card last) {
-        return hand.isEmpty()
-                   ? "Pair"
-                   : last.hasTheSameNumberAs(hand.head())
-                       ? "Two Pair"
-                       : rankForTwoPair(hand.tail(), hand.head());
-    }
+        private boolean rankForPair(final FunctionalList<Card> hand, final Card last) {
+            return hand.isEmpty()
+                       ? false
+                       : last.hasTheSameNumberAs(hand.head())
+                           ? true
+                           : rankForPair(hand.tail(), hand.head());
+        }
+    };
 
+    public static Predicate<FunctionalList<Card>> TWO_PAIR = new Predicate<FunctionalList<Card>>() {
+        @Override public boolean matches(final FunctionalList<Card> cards) {
+            return rankForPair(cards, Card.NULL);
+        }
+
+        private boolean rankForPair(final FunctionalList<Card> hand, final Card last) {
+            return hand.isEmpty()
+                       ? false
+                       : last.hasTheSameNumberAs(hand.head())
+                           ? rankForTwoPair(hand.tail(), Card.NULL)
+                           : false;
+        }
+
+        private boolean rankForTwoPair(final FunctionalList<Card> hand, final Card last) {
+            return hand.isEmpty()
+                       ? false
+                       : last.hasTheSameNumberAs(hand.head())
+                           ? true
+                           : rankForTwoPair(hand.tail(), hand.head());
+        }
+    };
 }
